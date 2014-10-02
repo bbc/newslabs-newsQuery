42c4cd673e1ccdb3911ed2dd01ce22fc63f15b53
Iain Collins <me@iaincollins.com>
2014-10-02 18:30:31 +0100

    Adding experimental undocumented feature
    Return articles similar to an arbitrary blob of text.
    
    This will DEFINITELY be changing in future, will document it when it's stable.

---
+++ test/get-similar-articles-from-text.js
@@ -0,0 +1,33 @@
+var assert = require("assert");
+var util = require("util");
+var moment = require("moment");
+var newsquery = require(__dirname+'/../lib/newsquery')(process.env.NEWSQUERY_API_KEY);
+
+describe('Get articles similar to supplied text', function(){
+    this.timeout(20000);
+    var response;
+
+    before(function(done){
+        newsquery.getSimilarArticlesFromText("Got my tax return back. Last year I paid £14,250 in income tax, £4,175 National Insurance, £1,620 council tax and e. £8,400 VAT #doingmybit")
+        .then(function(articles) {
+            response = articles;
+            done();
+        });
+    });
+
+    it('The list of articles should be an array', function(){
+        assert.equal((response instanceof Array), true);
+    });
+
+    it('Should return multiple articles', function(){
+        assert.equal((response.length > 1), true);
+    });
+
+    it('All articles should contain an id and a url', function(){
+        for (var i = 0; i < response.length; i++) {
+            assert.equal((typeof response[i].url === 'string'), true);
+            assert.equal((typeof response[i].id === 'string'), true);
+        }
+    });
+
+});
